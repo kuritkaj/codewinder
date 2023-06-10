b6994fdb9a6762f8e67d7bf2e4399732e4df1eeb
nyvyn <doc@firelord.net>
2023-06-10 13:13:48 -0500

    Can now pass environment variables to the Code Evaluator if they are prefixed with `VM_`.

--- lib/intelligence/tools/CodeEvaluator.ts
+++ lib/intelligence/tools/CodeEvaluator.ts
@@ -9,21 +9,30 @@ import { MemoryStore } from "@/lib/intelligence/memory/MemoryStore";
 import { GuardChain } from "@/lib/intelligence/chains/GuardChain";
 
 export const NAME = "code-evaluator";
-export const DESCRIPTION = `an AI-powered code evaluator.
+export const DESCRIPTION = `an AI-powered javascript evaluator.
 Always include complete, relevant details in the specification from previous observations and actions.
-Input is a code comments or a program specification for a function that will return the desired output as a string.
+The specification should never try to request inputs or placeholders; all details must be specified in the specification.
+This does not create a generic function, but instead should be special purpose to meet the provided specification.
 Input format:
 {{
   "action": "${ NAME }",
-  "action_input": "specification in natural language"
+  "action_input": "specification in natural language for javascript"
 }}`;
+
+const SPECIFICATION_INPUT = "specification";
+const ENVIRONMENT_INPUT = "environment";
+const EXAMPLE_INPUT = "example";
 
 export const GUIDANCE = `
 You are an AI assistant that is being provided a code specification.
 Your responsibility is to write the appropriate function to be run in a secure code environment, returning the result to the requestor.
 
 This is the code specification:
-{specification}
+{${SPECIFICATION_INPUT}}
+
+You have access to these environment variables:
+{${ENVIRONMENT_INPUT}}
+Access these environment variables like this: process.env['ENVIRONMENT_VARIABLE_NAME'].
 
 The code you write should take into account these considerations:
 * The environment the code will run in is an isolated Node.js environment with fetch() available.
@@ -34,7 +43,7 @@ The code you write should take into account these considerations:
 
 Here's a function you wrote similar to this in the past:
 \`\`\`javascript
-{example}
+{${EXAMPLE_INPUT}}
 \`\`\`
 
 Now, translate the natural language description into JavaScript code for immediate evaluation and return.
@@ -89,12 +98,28 @@ export class CodeEvaluator extends Tool {
 
     /** @ignore */
     async _call(specification: string): Promise<string> {
+        // Filter environment variables starting with "VM_"
+        const vmEnvironmentVariables = Object.entries(process.env)
+        .filter(([key]) => key.startsWith("VM_"))
+        .reduce((acc, [key, value]) => {
+            const strippedKey = key.replace("VM_", ""); // Remove the prefix
+            acc[strippedKey] = value;
+            return acc;
+        }, {});
+
+        // Generate a string of the keys that start with "VM_"
+        const vmEnvironmentKeys = Object.keys(vmEnvironmentVariables).join(", ");
+
         // Retrieve a similar example from memory.
-        const memory = await this.memory.retrieveSnippets(specification, 0.80);
-        const example = memory && memory.length > 0 ? memory[0].pageContent : "";
+        const memory = await this.memory.retrieve(specification, 0.80);
+        const example = memory && memory.length > 0 ? memory[0].pageContent : "No example found.";
 
         // Generate JavaScript code from the natural language description.
-        const completion = await this.llmChain.predict({ specification, example });
+        const completion = await this.llmChain.predict({
+            [SPECIFICATION_INPUT]: specification,
+            [ENVIRONMENT_INPUT]: vmEnvironmentKeys,
+            [EXAMPLE_INPUT]: example
+        });
 
         const regex = /(?<=```javascript)[\s\S]*?(?=\n```)/;
         const matches = completion.match(regex);
@@ -105,6 +130,9 @@ export class CodeEvaluator extends Tool {
             const output = vm.runInNewContext(match, {
                 console,
                 fetch,
+                process: {
+                    env: vmEnvironmentVariables
+                }
             }, { timeout: 3000 });
             const result = output ? await output : "No result returned.";
 
