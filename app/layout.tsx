92ce7190796eced1d05b39a432905edf3d8c5b2d
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-18 07:08:40 -0500

    Auto select dark/light mode based on browser/os preferences.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,42 @@ import { ReactNode } from "react";
 import '../scripts/wdyr';
 import "../styles/global.css";
 
+/** https://sreetamdas.com/blog/the-perfect-dark-mode */
+function setInitialColorMode() {
+    function getInitialColorMode() {
+        const preference = window.localStorage.getItem("light-dark-mode");
+        const hasExplicitPreference = Boolean(preference);
+        /**
+         * If the user has explicitly chosen light or dark,
+         * use it. Otherwise, this value will be null.
+         */
+        if (hasExplicitPreference) {
+            return preference;
+        }
+        // If there is no saved preference, use a media query
+        const mediaQuery = "(prefers-color-scheme: dark)";
+        const mql = window.matchMedia(mediaQuery);
+        const hasImplicitPreference = Boolean(mql);
+        if (hasImplicitPreference) {
+            return mql.matches ? "dark" : "light";
+        }
+        // default to 'dark'.
+        return "dark";
+    }
+    const colorMode = getInitialColorMode();
+    const root = document.documentElement;
+    root.style.setProperty("--initial-color-mode", colorMode);
+    // add HTML attribute
+    if (colorMode === "dark") document.documentElement.classList.add("dark-theme");
+    else document.documentElement.classList.add("light-theme");
+}
+// our function needs to be a string
+const blockingSetInitialColorMode = `(function() {
+		${setInitialColorMode.toString()}
+		setInitialColorMode();
+})()
+`;
+
 export const metadata: Metadata = {
     title: "Codewinder",
     description: "Your intelligent personal assistant",
@@ -14,7 +50,14 @@ export const metadata: Metadata = {
 function RootLayout({children}: { children: ReactNode }) {
     return (
         <html lang="en">
-        <body className={"dark-theme"}>{children}</body>
+        <body>
+            {children}
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: blockingSetInitialColorMode,
+                }}
+            ></script>
+        </body>
         </html>
     );
 }
