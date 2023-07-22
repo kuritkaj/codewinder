614ac7c9898dd69d6a82776c0f3360abe71a20a5
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-22 16:45:25 -0500

    Styling.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     return (
         <div className={styles.fullscreen}>
             <h2>Codewinder</h2>
-            <div className={styles.typewriter}><span className={styles.typetext}>A better <s>AI</s> way to get work done</span></div>
+            <div className={styles.typewriter}><span className={styles.typetext}>A better <s>way</s> AI to get work done</span></div>
             <Link href="/signin" prefetch={false}>
                 <Button className={styles.login}><h4>Get started</h4></Button>
             </Link>
