63679c6c96a9f34381842562f8a52b6d394ad943
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-23 06:41:28 -0500

    I'm pretty sure prefetching to the signout url was signing the user out right after logging in... *sigh*

--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = ({user}: HeaderProps) => {
             <div className={styles.navlogo}/>
             <div className={styles.navlinks}>
                 {user ? (
-                    <Link href="#" prefetch={false}>
+                    <Link href="/signout" prefetch={false}>
                         Sign out
                     </Link>
                 ) : (
