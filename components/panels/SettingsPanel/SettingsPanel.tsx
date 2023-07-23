2356dce2fe7eeb47b3f9c77acc73b83e84df8bdc
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-23 09:57:57 -0500

    Temporarily remove ability to use GPT4 until subscriptions are added.

--- components/panels/SettingsPanel/SettingsPanel.tsx
+++ components/panels/SettingsPanel/SettingsPanel.tsx
@@ -1,7 +1,6 @@
 import useSettings from "@/components/context/useSettings";
 import Button from "@/components/ui/common/Button";
 import { Cross1Icon } from "@radix-ui/react-icons";
-import * as Switch from '@radix-ui/react-switch';
 import styles from "./SettingsPanel.module.css";
 
 type SettingsProps = {
@@ -18,21 +17,21 @@ const SettingsPanel = ({onDelete}: SettingsProps) => {
     return (
         <div className={styles.panel}>
             <div className={styles.settingsleft}>
-                <div className={styles.switch}>
-                    <label className={styles.switchlabel} htmlFor="usePowerToggle">
-                        {usePower ? 'Power:' : 'Speed:'}
-                    </label>
-                    <Switch.Root
-                        aria-label="Toggle between power and speed modes"
-                        className={styles.switchroot}
-                        id="usePowerToggle"
-                        name="usePowerToggle"
-                        checked={usePower}
-                        onCheckedChange={handleUsePowerToggle}
-                    >
-                        <Switch.Thumb className={styles.switchthumb}/>
-                    </Switch.Root>
-                </div>
+                {/*<div className={styles.switch}>*/}
+                {/*    <label className={styles.switchlabel} htmlFor="usePowerToggle">*/}
+                {/*        {usePower ? 'Power:' : 'Speed:'}*/}
+                {/*    </label>*/}
+                {/*    <Switch.Root*/}
+                {/*        aria-label="Toggle between power and speed modes"*/}
+                {/*        className={styles.switchroot}*/}
+                {/*        id="usePowerToggle"*/}
+                {/*        name="usePowerToggle"*/}
+                {/*        checked={usePower}*/}
+                {/*        onCheckedChange={handleUsePowerToggle}*/}
+                {/*    >*/}
+                {/*        <Switch.Thumb className={styles.switchthumb}/>*/}
+                {/*    </Switch.Root>*/}
+                {/*</div>*/}
             </div>
             <div className={styles.divider}/>
             <div className={styles.settingsright}>
