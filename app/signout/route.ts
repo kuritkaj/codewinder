318a284c0e0eedba3d808fe36c40fbae108c1ff0
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-22 20:19:59 -0500

    Fix signout redirect

--- app/signout/route.ts
+++ app/signout/route.ts
@@ -5,7 +5,7 @@ import { type NextRequest, NextResponse } from 'next/server'
 export const dynamic = "force-dynamic"; // Workaround for NextJS bug https://github.com/vercel/next.js/issues/49373
 
 export async function GET(req: NextRequest) {
-    const SITE_URL = process.env.SITE_URL;
+    const SITE_URL= process.env.SITE_URL;
     const supabase = createRouteHandlerClient({ cookies });
 
     // Check if we have a session
