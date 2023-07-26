ee7997331c930f0a17214cb3839ccca266759504
nyvyn <2886602+nyvyn@users.noreply.github.com>
2023-07-25 20:13:38 -0500

    Comments.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
+import type { Database } from "@/lib/types/Database";
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
-import type { Database } from "@/lib/types/Database";
+import { NextResponse } from "next/server";
 
 async function middleware(req: NextRequest) {
     const res = NextResponse.next();
-    const supabase = createMiddlewareClient<Database>({ req, res });
+    const supabase = createMiddlewareClient<Database>({req, res});
+    // Refresh session if expired - required for Server Components
+    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
     await supabase.auth.getSession();
     return res;
 }
